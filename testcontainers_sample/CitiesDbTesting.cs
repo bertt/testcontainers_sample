48ca1eca7a44037ba4c295bcc9ea7cb30bb01bdc
Bert Temme <btemme@hotmail.com>
2024-06-21 16:57:50 +0200

    create and read cities database

--- testcontainers_sample/CitiesDbTesting.cs
+++ testcontainers_sample/CitiesDbTesting.cs
@@ -5,39 +5,53 @@ using Testcontainers.PostgreSql;
 namespace ConsoleAppTestContainers;
 public class CitiesDbTesting
 {
-    private PostgreSqlContainer containerPostgres;
+    private PostgreSqlContainer _containerPostgres;
 
     [OneTimeSetUp]
     public async Task Setup()
     {
-        containerPostgres = new PostgreSqlBuilder()
+        _containerPostgres = new PostgreSqlBuilder()
         .WithImage("postgis/postgis:16-3.4-alpine")
         .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(5432))
         .Build();
+        var initScript = File.ReadAllText("./postgres-db/create_cities.sql");
 
-        await containerPostgres.StartAsync().ConfigureAwait(false);
+        await _containerPostgres.StartAsync().ConfigureAwait(false);
+        await _containerPostgres.ExecScriptAsync(initScript).ConfigureAwait(false);
     }
 
     [OneTimeTearDown]
     public async Task TeardownOnce()
     {
-        await containerPostgres.StopAsync();
-        await containerPostgres.DisposeAsync(); 
+        await _containerPostgres.StopAsync();
+        await _containerPostgres.DisposeAsync(); 
     }
 
     [Test]
     public void TestCities()
     {
-        var connectionString = containerPostgres.GetConnectionString();
+        var connectionString = _containerPostgres.GetConnectionString();
         var connection = new Npgsql.NpgsqlConnection(connectionString);
         connection.Open();
 
         var command = connection.CreateCommand();
-        var sql = "SELECT PostGIS_Version()";
+        var sql = "select id, city_name, st_x(geom) as longitude, st_y(geom) as latitude from cities";
         command.CommandText = sql;
-        var version = command.ExecuteScalar();
+        var reader = command.ExecuteReader();
+        var cities = new List<City>();
+        while (reader.Read())
+        {
+            cities.Add(new City
+            {
+                Id = reader.GetInt32(0),
+                CityName = reader.GetString(1),
+                Longitude = reader.GetDouble(2),
+                Latitude = reader.GetDouble(3)
+            });
+        }
+        connection.Close();
 
-        Assert.That(version.Equals("3.4 USE_GEOS=1 USE_PROJ=1 USE_STATS=1"));
+        Assert.That(cities.Count.Equals(5));
         connection.Close();
     }
 }
